0e86535c699863847d29a08522ebcc98168a250e
Aishwarya Kumar <aishwarya17011@iiitd.ac.in>
2021-05-12 12:02:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let x = 20;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit src/App.js and save to reload.
         </p>
         <a
           className="App-link"
